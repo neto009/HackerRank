54ed4a21c2acadf7f99d5c87ba57a31defd18132
Neto <solangeneto001@gmail.com>
2022-10-10 12:49:08 -0300

    Ajuste

--- NewSolution.java
+++ NewSolution.java
@@ -5,7 +5,6 @@ public class NewSolution {
 
     public static void main(String[] args) throws IOException {
         List<List<String>> debts = new ArrayList<>();
-        int tamanho = 1;
         List<String> current1  = new ArrayList<>();
         List<String> current2  = new ArrayList<>();
         List<String> current3  = new ArrayList<>();
@@ -13,41 +12,31 @@ public class NewSolution {
         List<String> current5  = new ArrayList<>();
         List<String> current6  = new ArrayList<>();
 
-        for (int i = 0; i < tamanho; i++) {
-            current1.add(0, "Alex");
-            current1.add(1, "Blake");
-            current1.add(2, "2");
-        }
+        current1.add(0, "Alex");
+        current1.add(1, "Blake");
+        current1.add(2, "2");
 
-        for (int i = 0; i < tamanho; i++) {
-            current2.add(0, "Blake");
-            current2.add(1, "Alex");
-            current2.add(2, "2");
-        }
+        current2.add(0, "Blake");
+        current2.add(1, "Alex");
+        current2.add(2, "2");
 
-        for (int i = 0; i < tamanho; i++) {
-            current3.add(0, "Casey");
-            current3.add(1, "Alex");
-            current3.add(2, "5");
-        }
 
-        for (int i = 0; i < tamanho; i++) {
-            current4.add(0, "Blake");
-            current4.add(1, "Casey");
-            current4.add(2, "7");
-        }
+        current3.add(0, "Casey");
+        current3.add(1, "Alex");
+        current3.add(2, "5");
 
-        for (int i = 0; i < tamanho; i++) {
-            current5.add(0, "Alex");
-            current5.add(1, "Blake");
-            current5.add(2, "4");
-        }
 
-        for (int i = 0; i < tamanho; i++) {
-            current6.add(0, "Alex");
-            current6.add(1, "Casey");
-            current6.add(2, "4");
-        }
+        current4.add(0, "Blake");
+        current4.add(1, "Casey");
+        current4.add(2, "7");
+
+        current5.add(0, "Alex");
+        current5.add(1, "Blake");
+        current5.add(2, "4");
+
+        current6.add(0, "Alex");
+        current6.add(1, "Casey");
+        current6.add(2, "4");
 
         debts.add(current1);
         debts.add(current2);
@@ -72,6 +61,7 @@ public class NewSolution {
         Map<String, Integer> finalMap = new HashMap<>();
         for(int i = 0; i < debts.size(); i++) {
             finalMap.put(debts.get(i).get(0), 0);
+            finalMap.put(debts.get(i).get(1), 0);
         }
 
         for(int i = 0; i < debts.size(); i++) {
